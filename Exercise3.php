7d108b29cbbcf63ba62762110e5da7ad65630e1e
JoshuaB09 <joshua09balagbagan@gmail.com>
2022-09-21 11:28:07 +0800

    php exercise

---
+++ Exercise3.php
@@ -0,0 +1,29 @@
+<?php
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+ $x=8; 
+
+ echo "Value is now " . $x . "\n";
+ $x += 2;
+ echo "Add 2. Value is now " . $x . "\n";
+ $x -= 4;
+ echo "Subtract by 4. Value is now " . $x . "\n";
+ $x *= 5;
+ echo "Multiply by 5. Value is now " . $x . "\n";
+ $x /= 3;
+ echo "Divide by 3. Value is now " . $x  . "\n";
+ $x = ++$x;
+ echo "Increment value by one. Value is now " . $x  . "\n";
+ $x = --$x;
+ echo "Decrement value by one. Value is now " . $x  . "\n";
+
+
+ ?>
---
+++ Exercise4.php
@@ -0,0 +1,26 @@
+<?php
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+$name='Harry'; 
+$age=28;   
+
+var_dump($name); 
+echo "";
+
+print_r($name);
+echo "\n";   
+ 
+ var_dump($age); 
+echo "";
+  
+$name=NULL;   
+var_dump($name);
+
+?> 
